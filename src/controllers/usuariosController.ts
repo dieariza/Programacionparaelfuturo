6c11c7cdbaebec7e9af255ff3316718e7c490a43
Ricardo Toloza <ricardotoloza7@gmail.com>
2021-11-17 18:01:22 -0500

    rutas

---
+++ src/controllers/usuariosController.ts
@@ -0,0 +1,20 @@
+
+const obtenerUsarios = (req, res) => {
+    res.send('obtenerUsuarios');
+}
+const crearUsuario= (req, res) => {
+    res.send('crearUsuario');
+}
+const actualizarUsuario = (req, res) => {
+    res.send('actualizarUsuario');
+}
+const eliminarUsuario= (req, res) => {
+    res.send('eliminarUsuario');
+}
+const buscarUsuario = (req, res) => {
+    res.send('buscarUsuario');
+}
+
+export { obtenerUsarios, crearUsuario, actualizarUsuario, eliminarUsuario, buscarUsuario}
+
+
